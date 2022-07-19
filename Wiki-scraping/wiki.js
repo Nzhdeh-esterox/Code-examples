87beaa13ccad1e6c91b213729d670c5370a96382
Nzhdeh Barseghyan <nzhdeh.barseghyan@esterox.am>
2022-07-19 13:29:43 +0400

    Scrape list data from the Wikipedia page.

---
+++ Wiki-scraping/wiki.js
@@ -0,0 +1,36 @@
+// Core Modules
+const fs = require('fs');
+
+// Third-party Modules
+const wiki = require('wikijs').default;
+const {parse} = require('node-html-parser');
+
+const articleName = 'List of people from Phoenix';
+
+
+wiki().find(articleName).then(async (page) => {
+  const sections = await page.content();
+  const data = await page.html();
+
+  for (let section of sections) {
+    const root = parse(data);
+    const title = section.title;
+
+    const parent = root.querySelector(`#${title}`)?.parentNode;
+
+    if (!parent) {
+      continue;
+    }
+
+    const list = parent.querySelector('~ div ul');
+    section.content = list?.innerHTML;
+  }
+
+  fs.writeFile('./test.json', JSON.stringify(sections), (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('File written successfully!');
+  })
+});
